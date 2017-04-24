cbb2c3d7968680bd993ff776d651ff034462433d
kohlbrr <kohlbrr@gmail.com>
2017-04-24 18:29:37 -0400

    Continuing work on app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.method + ' '  + req.path)
+  next();
+})
+
+app.get('/', (req, res) => res.send('It.. is.. ALIVE!\n'))
+
+app.listen(8080, () => console.log('App listening on 8080!'))
+
