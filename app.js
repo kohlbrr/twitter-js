3255e073a547f63e8e48a28fa9fcaa43b3dc09b2
kohlbrr <kohlbrr@gmail.com>
2017-04-25 10:12:43 -0400

    Forgot to commit yesterday

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+// We'll use `morgan` here if need be
 
 app.use((req, res, next) => {
-  console.log(req.method + ' '  + req.path)
+  console.log(req.method + ' '  + req.path + ' ' + res.statusCode)
   next();
 })
 
-app.get('/', (req, res) => res.send('It.. is.. ALIVE!\n'))
+app.use('/special', (req, res, next) => {
+  console.log('You made it!')
+})
+
+app.get('/', (req, res) => { 
+  res.status(201).send('Hi!');
+})
 
 app.listen(8080, () => console.log('App listening on 8080!'))
 
