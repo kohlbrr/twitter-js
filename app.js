eafcec9a1424d8fa138fee80c8508be4788bcf90
kohlbrr <kohlbrr@gmail.com>
2017-04-25 10:50:50 -0400

    Soved logging issue

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const app = express();
 // We'll use `morgan` here if need be
 
 app.use((req, res, next) => {
-  console.log(req.method + ' '  + req.path + ' ' + res.statusCode)
+  var logMessage = req.method + ' ' + req.path
+  res.on('finish', () => {
+    logMessage += ' ' + res.statusCode
+    console.log(logMessage)
+  })
   next();
 })
 
 app.use('/special', (req, res, next) => {
   console.log('You made it!')
+  next();
 })
 
-app.get('/', (req, res) => { 
-  res.status(201).send('Hi!');
+app.get('/', (req, res, next) => { 
+  res.status(201).send('Hi!\n');
 })
 
 app.listen(8080, () => console.log('App listening on 8080!'))
-
