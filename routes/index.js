d5d5e725c873781079c91829bbd624a41bdbae4d
Howard Jian <hcwjian@gmail.com>
2017-04-25 14:59:43 -0400

    up to socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 const express = require('express'),
       router = express.Router(),
-      tweetBank = require('../tweetBank');
+      tweetBank = require('../tweetBank'),
+      parser = require('body-parser');
 
 router.get('/',function(req,res) {
   let tweets = tweetBank.list();
-  res.render('index',{tweets: tweets} );
+  res.render('index',{tweets: tweets, showForm: true});
 });
 
-router.get('/users/:id', (req, res) => {
-  var id = Number(req.params.id);
-  var list = tweetBank.find({id: id});
-  res.render('index', {tweets: list});
+// router.get('/users/:id', (req, res) => {
+//   var id = Number(req.params.id);
+//   var list = tweetBank.find({id: id});
+//   res.render('index', {tweets: list, showForm: false});
+// });
+
+router.get('/users/:name', (req, res) => {
+  var name = req.params.name;
+  var list = tweetBank.find({name: name});
+  res.render('index', {tweets: list, showForm: true, name: name});
 });
 
-router.get('/tweets/:id', (req, res) => {
-  
-});
+// router.get('/tweets/:id', (req, res) => {
+//
+// });
+
+router.post('/tweets',function (req,res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+})
 
 module.exports = router;
