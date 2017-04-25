d06958bbd0c537e56e6c0eb1a301a407a07a3b90
kohlbrr <kohlbrr@gmail.com>
2017-04-25 13:10:18 -0400

    Added static functionality

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,17 @@ const express = require('express'),
 
 router.get('/',function(req,res) {
   let tweets = tweetBank.list();
+  console.log(tweets)
   res.render('index',{tweets:tweets} );
 });
 
-
+router.get('/users/:name', (req, res) => {
+  var name = req.params.name;
+  var list = tweetBank.find({name: name});
+  //Nimit Somethingelse
+  console.log(name)
+  console.log(list)
+  res.render('index', {tweets: list});
+})
 
 module.exports = router;
